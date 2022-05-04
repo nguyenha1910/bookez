5cce551d44834109a6d3e34321c1399e78159581
nguyenha <nguyenhbhcm@gmail.com>
2022-05-04 17:36:15 -0400

    Fixing book detail link

--- public/js/book_list.js
+++ public/js/book_list.js
@@ -5,13 +5,19 @@
 
 function get_book_object(book) {
     return `<div class="bookDiv row py-2" data-m="${book._id}">
-    <div class="col-1 img"><a><img id="book_list_img" class="book_list_img" src="img/dummy_book.png" alt="square"/></a></div>
-                <div class="col-7 book_name"><a>${book.book_name}</a></div>
-                <div class="col-2 author_name"><a>${book.author_name}</div>
-                <div class="col-1 price"><a>$ ${book.price.toFixed(2)}</a></div>
-<div class="buttonDiv col-2">
-    <button type="button" class="btn btn-outline-primary">Add to Cart</button>
-</div>
+                <div class="imgDiv" onclick="toDetailPage(this)">                
+                    <div class="col-1 img"><a><img id="book_list_img" 
+                    class="book_list_img" src="img/dummy_book.png" alt="book image"/></a>
+                    </div>
+                </div>
+                <div class="infoDiv" onclick="toDetailPage(this)">
+                    <div class="col-7 book_name"><a>${book.book_name}</a></div>
+                    <div class="col-2 author_name"><a>${book.author_name}<a></div>
+                    <div class="col-1 price">$ ${book.price.toFixed(2)}</div>
+                </div>
+                <div class="buttonDiv" onclick="addToCart(this)">
+                    <button type="button" class="btn btn-outline-primary">Add to Cart</button>
+                </div>
             </div>`;
 }
 
@@ -23,12 +29,10 @@ function showList(books) {
 
     ///
     $('#book_list li')
-    .attr("value", function (idx) {
-        return books[idx]._id;
-    })
-    .append("<div class='row'></div>");
-
-    ///
+        .attr("value", function (idx) {
+            return books[idx]._id;
+        })
+        .append("<div class='row'></div>");
 
     $.each($('#book_list .row'), function (idx) {
         $(this).addClass('col-lg-4 col-md-5 col-sm-8');
@@ -38,28 +42,9 @@ function showList(books) {
             $(this).addClass('odd_row');
         }
     });
-
-
-
-    $('.infoDiv').on('click', function () {
-        const book_id = $(this).parent().attr('data-m');
-        location.href = "views/book_detail.html?book_id=" + book_id;
-    });
-    $('.buttonDiv').on('click', function () {
-        const book_id = $(this).parent().attr('data-m');
-        $.post('/add_to_cart', {book_id: book_id}).done((data) => {
-            if (data.message === "success"){
-                console.log(data);
-                location.reload();
-            } else{
-                console.log(data);
-                location.href = data.redr;
-            }
-        });
-    });
 }
 
-$.getJSON("/get_all_books").done((data)=>{
+$.getJSON("/get_all_books").done((data) => {
     if (data.message === "success") {
         console.log("file loaded");
         bookData = data.data;
@@ -79,13 +64,13 @@ function sortAlpha(prop, a, b) {
     return 0;
 }
 
-function update_books(){
+function update_books() {
     const sort_by = $('#sortDropdown').val();
-    if (sort_by === "book_name_sort"){
+    if (sort_by === "book_name_sort") {
         bookData.sort((a, b) => sortAlpha("book_name", a, b));
-    } else if (sort_by === "author_name_sort"){
+    } else if (sort_by === "author_name_sort") {
         bookData.sort((a, b) => sortAlpha("author_name", a, b));
-    } else{
+    } else {
         bookData.sort(function (a, b) {
             return a.price - b.price;
         });
@@ -96,10 +81,29 @@ function update_books(){
 function searchBook() {
     $.get('/search_books', {
         search_key: $('#search_box').val(),
-    }).done((data)=>{
+    }).done((data) => {
         console.log(data);
-        if (data.message === "success"){
+        if (data.message === "success") {
             showList(data.data);
+        }
+    });
+}
+
+function toDetailPage(divObj) {
+    const book_id = $(divObj).parent().attr('data-m');
+    location.href = "views/book_detail.html?book_id=" + book_id;
+}
+
+function addToCart(divObj) {
+    console.log('Add to cart clicked');
+    const book_id = $(addToCart).parent().attr('data-m');
+    $.post('/add_to_cart', {book_id: book_id}).done((data) => {
+        if (data.message === "success") {
+            console.log(data);
+            location.reload();
+        } else {
+            console.log(data);
+            location.href = data.redr;
         }
     });
 }
