a7ea8a0bf77e8cb9d6421ffcaa470a220316b75d
minhvq0899 <minhvq0899@gmail.com>
2022-05-11 18:21:27 -0400

    checng server.js

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const path = require('path');
 const dotenv = require('dotenv'); 
 const cookieParser = require('cookie-parser'); 
 const mongoose = require('mongoose');
+const csv = require('csv-parser');
+const fs = require('fs');
 var session = require('cookie-session');
 //Add sessions
 // const session = require('express-session');
@@ -23,6 +25,38 @@ mongoose.connect('mongodb://localhost:27017/bookDB',
     {useNewUrlParser: true}, function () {
         console.log("db connection successful");
     });
+
+// ------------------------------------------------
+// const bookSchema = {
+//     book_name: String,
+//     author_name: String,
+//     price: { type: Number, default: 0 },
+// };
+
+// const Book = mongoose.model('Book', bookSchema);
+
+// const bookList = [];
+
+// fs.createReadStream(__dirname+'/data/bookez_data.csv')
+//     .pipe(csv())
+//     .on('data', (data) => bookList.push(data))
+//     .on('end', () => {
+//         console.log(bookList[0]);
+//         // Do the database insertMany here
+//         Book.insertMany(bookList, {}, function (err) {
+//             if (err) {
+//                 console.log(err);
+//             } else {
+//                 console.log("All data loaded");
+//                 mongoose.connection.close();
+//             }
+//         });
+//     });
+//--------------------------------------------------
+
+
+
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static( path.join(__dirname, './public') ));
