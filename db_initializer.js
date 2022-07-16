82cda1f8500ac82c105fba51eefa05622b5fccea
nguyenha <nguyenhbhcm@gmail.com>
2022-07-15 23:48:14 -0400

    trying to deploy to heroku

--- db_initializer.js
+++ db_initializer.js
@@ -1,36 +1,47 @@
+const dotenv = require('dotenv').config();
 const mongoose = require('mongoose');
 const csv = require('csv-parser');
 const fs = require('fs');
+// const dotenv = require("dotenv");
 
-// const rawdata = fs.readFileSync(__dirname + "data/bookez_data.csv");
+// dotenv.config({ path: './.env'});
 
-mongoose.connect('mongodb://localhost:27017/bookDB',
+
+const rawdata = fs.readFileSync(__dirname + "/data/bookez_data.csv");
+
+const uri = process.env.MONGODB_URI;
+const uri = 'mongodb://localhost:27017/bookDB';
+console.log(uri);
+
+mongoose.connect(uri,
     {useNewUrlParser: true}, function () {
         console.log("db connection successful");
     });
 
-const bookSchema = {
-    book_name: String,
-    author_name: String,
-    price: { type: Number, default: 0 },
-};
-
-const Book = mongoose.model('Book', bookSchema);
-
-const bookList = [];
-
-fs.createReadStream(__dirname+'/data/bookez_data.csv')
-    .pipe(csv())
-    .on('data', (data) => bookList.push(data))
-    .on('end', () => {
-        console.log(bookList[0]);
-        // Do the database insertMany here
-        Book.insertMany(bookList, {}, function (err) {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log("All data loaded");
-                mongoose.connection.close();
-            }
-        });
-    });
+// const bookSchema = {
+//     book_name: String,
+//     author_name: String,
+//     price: { type: Number, default: 0 },
+// };
+//
+// const Book = mongoose.model('Book', bookSchema);
+//
+// const bookList = [];
+//
+// fs.createReadStream(__dirname+'/data/bookez_data.csv')
+//     .pipe(csv())
+//     .on('data', (data) => bookList.push(data))
+//     .on('end', () => {
+//         console.log(bookList[0]);
+//         // Do the database insertMany here
+//         Book.insertMany(bookList, {}, function (err) {
+//             if (err) {
+//                 console.log(err);
+//             } else {
+//                 console.log("All data loaded");
+//                 mongoose.connection.close();
+//             }
+//         });
+//     });
+//
+//
--- server.js
+++ server.js
@@ -17,46 +17,16 @@ dotenv.config({ path: './.env'});
 const app = express();
 
 // for deployment
-// require('./middleware/production')(app); 
+// require('./middleware/production')(app);
+
 const uri = process.env.MONGODB_URI;
+const uri = 'mongodb://localhost:27017/bookDB';
 console.log(uri);
 
-mongoose.connect('mongodb://localhost:27017/bookDB',
+mongoose.connect(uri,
     {useNewUrlParser: true}, function () {
         console.log("db connection successful");
     });
-
-// ------------------------------------------------
-// const bookSchema = {
-//     book_name: String,
-//     author_name: String,
-//     price: { type: Number, default: 0 },
-// };
-
-// const Book = mongoose.model('Book', bookSchema);
-
-// const bookList = [];
-
-// fs.createReadStream(__dirname+'/data/bookez_data.csv')
-//     .pipe(csv())
-//     .on('data', (data) => bookList.push(data))
-//     .on('end', () => {
-//         console.log(bookList[0]);
-//         // Do the database insertMany here
-//         Book.insertMany(bookList, {}, function (err) {
-//             if (err) {
-//                 console.log(err);
-//             } else {
-//                 console.log("All data loaded");
-//                 mongoose.connection.close();
-//             }
-//         });
-//     });
-//--------------------------------------------------
-
-
-
-
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static( path.join(__dirname, './public') ));
