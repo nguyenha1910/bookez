017d186f50ae19622da2deffdd767a8a8c732f9f
nguyenha <nguyenhbhcm@gmail.com>
2022-07-15 23:55:29 -0400

    Initialize DB

--- db_initializer.js
+++ db_initializer.js
@@ -18,30 +18,30 @@ mongoose.connect(uri,
         console.log("db connection successful");
     });
 
-// const bookSchema = {
-//     book_name: String,
-//     author_name: String,
-//     price: { type: Number, default: 0 },
-// };
-//
-// const Book = mongoose.model('Book', bookSchema);
-//
-// const bookList = [];
-//
-// fs.createReadStream(__dirname+'/data/bookez_data.csv')
-//     .pipe(csv())
-//     .on('data', (data) => bookList.push(data))
-//     .on('end', () => {
-//         console.log(bookList[0]);
-//         // Do the database insertMany here
-//         Book.insertMany(bookList, {}, function (err) {
-//             if (err) {
-//                 console.log(err);
-//             } else {
-//                 console.log("All data loaded");
-//                 mongoose.connection.close();
-//             }
-//         });
-//     });
-//
-//
+const bookSchema = {
+    book_name: String,
+    author_name: String,
+    price: { type: Number, default: 0 },
+};
+
+const Book = mongoose.model('Book', bookSchema);
+
+const bookList = [];
+
+fs.createReadStream(__dirname+'/data/bookez_data.csv')
+    .pipe(csv())
+    .on('data', (data) => bookList.push(data))
+    .on('end', () => {
+        console.log(bookList[0]);
+        // Do the database insertMany here
+        Book.insertMany(bookList, {}, function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("All data loaded");
+                mongoose.connection.close();
+            }
+        });
+    });
+
+
